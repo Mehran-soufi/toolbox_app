967f76fb07d4c7d559d75f789d6147198918f7c3
mehran-soufi <mehransoufi33@gmail.com>
2026-07-02 12:20:45 +0330

    edit footer for berter design

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -44,22 +44,22 @@ function Footer() {
   return (
     <footer
       className="w-full select-none rounded-xl py-3 px-3
-     bg-linear-to-b from-violet-600 to-purple-700
-     shadow-inner shadow-cyan-400"
+    bg-transparent
+     shadow-inner shadow-sky-700"
     >
-      <div className="w-full flex flex-wrap items-center justify-between gap-x-8">
+      <div className="w-full flex xl:flex-row flex-col items-center justify-between gap-x-8">
         {footerItem.map((item, index) => {
           const Icon = item.icon;
 
           return (
             <div
-              className="flex flex-1 items-center justify-between py-3 xl:py-0 xl:pl-4
-               border-b xl:border-b-0 xl:border-l border-slate-300"
+              className="flex w-full xl:w-auto flex-1 items-center justify-between py-3 xl:py-0 xl:pl-4
+               border-b xl:border-b-0 xl:border-l border-sky-700 last:border-none"
               key={index}
             >
               <div className="flex flex-col items-center justify-center gap-y-1">
-                <p className="text-white">{item.title}</p>
-                <span className="text-sm text-gray-300">
+                <p className="">{item.title}</p>
+                <span className="text-sm text-gray-500">
                   {item.description}
                 </span>
               </div>
